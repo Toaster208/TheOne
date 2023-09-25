40c64cbc79778c28dd8fe89bafcf3a94b9792340
Arjun Thosar <arjunthosar@gmail.com>
2023-09-25 13:18:17 -0500

    start

---
+++ src/GameWindow.java
@@ -0,0 +1,48 @@
+
+import javax.swing.JPanel;
+
+
+public class GameWindow extends JPanel implements Runnable {
+
+
+
+    public void run() {
+        //code
+    }
+
+    // public GameWindow() {
+    //     super("Drawing");
+
+    //     setSize(500, 500);
+    //     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     setLocationRelativeTo(null);
+    // }
+
+    // public void paint(Graphics g) {
+    //     super.paint(g);
+    // }
+
+
+    // public static void main(String[] args) {
+    //     new GameWindow().setVisible(true);
+    // }
+
+    // public void changeScale (double scale) {
+    //     s = scale;
+    //     refresh();
+    // }
+
+    // public void moveX (double dist) {
+    //     x += dist;
+    //     refresh();
+    // }
+
+    // public void moveY (double dist) {
+    //     y += dist;
+    //     refresh();
+    // }
+
+    // public void refresh() {
+    //     super.repaint();
+    // }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    // public double s = .25;
+    // public static double x;
+    // public static double y;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        
+        frame.setVisible(true);
+
+        
+    }
+
+    // public void drawLines(Graphics g) {
+    //     Graphics2D g2d = (Graphics2D) g;
+    //     g2d.fillOval((int) (225*s+x), (int) (225*s+y), (int) (50*s), (int) (50*s));
+    //     g2d.setStroke(new BasicStroke((int) (5*s)));
+    //     g2d.drawLine((int) (250*s+x), (int) (250*s+y), (int) (250*s+x), (int) (350*s+y));
+    //     g2d.drawLine((int) (250*s+x), (int) (285*s+y), (int) (225*s+x), (int) (300*s+y));
+    //     g2d.drawLine((int) (250*s+x), (int) (285*s+y), (int) (275*s+x), (int) (300*s+y));
+    //     g2d.drawLine((int) (250*s+x), (int) (350*s+y), (int) (225*s+x), (int) (400*s+y));
+    //     g2d.drawLine((int) (250*s+x), (int) (350*s+y), (int) (275*s+x), (int) (400*s+y));
+    // }
+
+    
+}
