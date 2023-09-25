6f50c933721b7750d883bf09fde802946e19850a
Arjun Thosar <arjunthosar@gmail.com>
2023-09-25 14:34:15 -0500

    working

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,48 +1,85 @@
 
+import java.awt.*;
+
 import javax.swing.JPanel;
 
 
 public class GameWindow extends JPanel implements Runnable {
 
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
 
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
-    public void run() {
-        //code
+    public GameWindow() {
+        this.setPreferredSize(new Dimension(768, 576));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
-    // public GameWindow() {
-    //     super("Drawing");
+    public void startGameThread() {
+        this.gameThread = new Thread(this);
+        gameThread.start();
+    }
 
-    //     setSize(500, 500);
-    //     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //     setLocationRelativeTo(null);
-    // }
+    public void run() {
+        double drawInterval = 1000000000 / 60;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
-    // public void paint(Graphics g) {
-    //     super.paint(g);
-    // }
+        long timer = 0;
+        int drawCount = 0;
 
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-    // public static void main(String[] args) {
-    //     new GameWindow().setVisible(true);
-    // }
+            if (delta >= 1) {
+                // 1: Update information, such as character position
+                update();
 
-    // public void changeScale (double scale) {
-    //     s = scale;
-    //     refresh();
-    // }
+                // 2: Draw the screen with updated information
+                repaint(); // Calls the paintComponent() method
 
-    // public void moveX (double dist) {
-    //     x += dist;
-    //     refresh();
-    // }
+                delta--;
+                drawCount++;
+            }
 
-    // public void moveY (double dist) {
-    //     y += dist;
-    //     refresh();
-    // }
+            if (timer >= 1000000000) {
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
 
-    // public void refresh() {
-    //     super.repaint();
-    // }
+    public void update() {
+        if (keyHandler.upPressed) {
+            playerY -= playerSpeed;
+        } 
+        if (keyHandler.downPressed) {
+            playerY += playerSpeed;
+        }
+        if (keyHandler.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        if (keyHandler.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.white);
+        g2d.fillOval(playerX, playerY, 48, 48);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,19 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("The One");
+
+        GameWindow gameWindow = new GameWindow();
+        frame.add(gameWindow);
+
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        
+        gameWindow.startGameThread();
     }
 
     // public void drawLines(Graphics g) {
